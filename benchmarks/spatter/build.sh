8f9cccb8569203e7e0e8355adb7cdd7bfa52ba85
Alireza Khadem <arkhadem@umich.edu>
2025-03-27 14:01:44 -0400

    build fixed

--- benchmarks/spatter/build.sh
+++ benchmarks/spatter/build.sh
@@ -1,5 +1,6 @@
 bash compile.sh GEM5
 echo "############## Setting Up XRAGE Dataset ##############"
+rm xrage.tar.gz*
 wget https://web.eecs.umich.edu/~arkhadem/projects/xrage.tar.gz
 mkdir -p tests/test-data/xrage/
 tar -xzf xrage.tar.gz -C tests/test-data/xrage/
