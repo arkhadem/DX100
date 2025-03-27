a76b4861b067de3a01018713f7123662c6e33a61
Alireza Khadem <arkhadem@umich.edu>
2025-03-27 13:18:24 -0400

    XRAGE setup for spatter added

--- benchmarks/spatter/build.sh
+++ benchmarks/spatter/build.sh
@@ -1,1 +1,5 @@
 bash compile.sh GEM5
+echo "############## Setting Up XRAGE Dataset ##############"
+wget https://web.eecs.umich.edu/~arkhadem/projects/xrage.tar.gz
+mkdir -p tests/test-data/xrage/
+tar -xzf xrage.tar.gz -C tests/test-data/xrage/
